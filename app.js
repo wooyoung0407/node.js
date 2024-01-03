38e75a74361586fc48859a934dfa61b0d9641c6e
wooyoung0407 <wooyoung0407@naver.com>
2024-01-03 21:04:50 +0900

    출력/id출력/notfound/return

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 import express from "express";
+import mysql from "mysql2/promise";
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "wise_saying",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 
 const app = express();
 const port = 3000;
@@ -14,10 +25,28 @@ const wiseSayings = [
   },
 ];
 
-app.get("/wise-sayings", (req, res) => {
-  res.json(wiseSayings);
+app.get("/wise-sayings", async (req, res) => {
+  const [rows] = await pool.query("SELECT * FROM wise_saying ORDER BY id DESC");
+
+  res.json(rows);
+});
+
+app.get("/wise-sayings/:id", async (req, res) => {
+  const { id } = req.params;
+  const [rows] = await pool.query("SELECT * FROM wise_saying WHERE id = ?", [
+    id,
+  ]);
+
+  if(rows.length==0){
+    res.status(404).send('not found');
+    return;
+  }
+
+
+
+  res.json(rows[0]);
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-}); 
+});
